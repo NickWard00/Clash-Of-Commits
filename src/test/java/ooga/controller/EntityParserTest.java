9e214cc56b0390470d1e3dee075b6989b53429ed
Nick Ward <nw121@duke.edu>
2022-11-11 18:07:52 -0500

    new parser for entity and tests issue #8

---
+++ src/test/java/ooga/controller/EntityParserTest.java
@@ -0,0 +1,16 @@
+package ooga.controller;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class EntityMapParserTest {
+    @Test
+    void testEntityMapParser() {
+        EntityMapParser entityMapParser = new EntityMapParser(new File("data/EntityMap.sim"));
+        assertEquals("EntityMap", "EntityMap");
+
+    }
+}
