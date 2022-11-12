771f859dda64ab5f525e497e4f4e9772ee63c211
Nick Ward <nw121@duke.edu>
2022-11-12 16:19:54 -0500

    model now is initalized in controller and gets list of entities

--- src/main/java/ooga/model/Model.java
+++ src/main/java/ooga/model/Model.java
@@ -1,4 +1,14 @@
 package ooga.model;
 
+import ooga.controller.Controller;
+
+import java.util.List;
+
 public class Model {
+    private Controller myController;
+    private List<Entity> myEntities;
+    public Model(Controller controller) {
+        myController = controller;
+        myEntities = myController.getEntities();
+    }
 }
