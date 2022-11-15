2a962fb1669a04507718552ac91d443132fb409c
Nicki Lee <nol3@duke.edu>
2022-11-15 00:41:55 -0500

    #1 added tests for entities, #6 test for enemies, #16 tests for attacks

--- src/test/java/ooga/model/attack/AttackTest.java
+++ src/test/java/ooga/model/attack/AttackTest.java
@@ -3,7 +3,9 @@ package ooga.model.attack;
 import ooga.controller.EntityParser;
 import ooga.model.Entity;
 import ooga.model.enemy.Bug;
+import ooga.model.enemy.MagicValue;
 import ooga.model.hero.MainHero;
+import org.assertj.core.util.VisibleForTesting;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -12,15 +14,19 @@ import static org.junit.jupiter.api.Assertions.*;
 class AttackTest {
 
     EntityParser bugParser;
+    EntityParser magicValueParser;
     EntityParser heroParser;
     Entity testBug;
+    Entity testMagicValue;
     Entity testHero;
 
     @BeforeEach
     void setUp() {
         bugParser = new EntityParser("TestBug", new String[]{"Bug", "0", "0"});
+        magicValueParser = new EntityParser("TestMagicValue", new String[]{"MagicValue", "0", "0"});
         heroParser = new EntityParser("TestHero", new String[]{"MainHero", "0", "0"});
         testBug = new Bug(bugParser.getAttributeMap());
+        testMagicValue = new MagicValue(magicValueParser.getAttributeMap());
         testHero = new MainHero(heroParser.getAttributeMap());
     }
 
@@ -35,4 +41,15 @@ class AttackTest {
         Attack testAttack = new LongRange(testHero);
         assertEquals(testHero, testAttack.getMyEntity());
     }
-}
+
+    @Test
+    void attackTest_LongRange() {
+        Attack testLongRange = Attack.attack(testMagicValue);
+        assertInstanceOf(LongRange.class, testLongRange);
+    }
+
+    @Test
+    void attackTest_ShortRange() {
+        Attack testShortRange = Attack.attack(testBug);
+        assertInstanceOf(ShortRange.class, testShortRange);
+    }}
