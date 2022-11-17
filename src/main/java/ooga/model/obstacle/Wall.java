b56d0cb5b8e1bc4ea1a804ed579b73184fc4938a
James Qu <jq46@duke.edu>
2022-11-17 02:21:23 -0500

    Handled obstacle collisions

--- src/main/java/ooga/model/obstacle/Wall.java
+++ src/main/java/ooga/model/obstacle/Wall.java
@@ -1,6 +1,8 @@
 package ooga.model.obstacle;
 
+import ooga.model.Entity;
 import ooga.model.obstacle.Obstacle;
+import ooga.model.state.MovementState;
 
 /**
  * This superclass represents all obstacles that will block movement or projectiles in general
@@ -11,5 +13,11 @@ public abstract class Wall extends Obstacle {
 
   public Wall(Double x, Double y, boolean canBeDestroyed, boolean onScreen) {
     super(x, y, true, canBeDestroyed, onScreen);
+  }
+
+  public void block(Entity entity) {
+    if (this.onScreen) {
+      entity.changeMovement(MovementState.STATIONARY);
+    }
   }
 }
