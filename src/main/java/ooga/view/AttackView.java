2a27220bd0066ff0de1e04ea500fa1390164207f
Nick Ward <nw121@duke.edu>
2022-11-21 16:08:18 -0500

    added attackview and tests

---
+++ src/main/java/ooga/view/AttackView.java
@@ -0,0 +1,102 @@
+package ooga.view;
+
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import ooga.model.state.DirectionState;
+
+import java.lang.reflect.InvocationTargetException;
+
+/**
+ * @author Nick Ward, Melanie Wang
+ */
+public class AttackView extends ImageView {
+    private String attackType;
+    private String imagePath;
+    private Image northSprite;
+    private Image southSprite;
+    private Image westSprite;
+    private Image eastSprite;
+
+    /**
+     * Creates a new AttackView with the given image path, attack type, x & y position, and size
+     * @param imagePath
+     * @param direction
+     * @param xPosition
+     * @param yPosition
+     * @param xSize
+     * @param ySize
+     */
+    public AttackView(String imagePath, DirectionState direction, String attackType, double xPosition, double yPosition, int xSize, int ySize) {
+        super(new Image(String.format("%s%s_%s.png", imagePath, attackType, direction.getDirection())));
+        this.imagePath = imagePath;
+        this.attackType = attackType;
+        setupSprites();
+        this.setX(xPosition);
+        this.setY(yPosition);
+        this.setFitWidth(xSize);
+        this.setFitHeight(ySize);
+    }
+
+    /**
+     * Sets the sprite to the corresponding direction
+     * @param newDirection the new direction of the sprite
+     */
+    public void changeDirection(DirectionState newDirection) {
+        String spriteDirection = newDirection.getDirection();
+        try {
+            this.getClass().getDeclaredMethod(String.format("set%sSprite", spriteDirection)).invoke(this);
+        } catch (IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
+            throw new IllegalStateException("methodNotFound", e);
+        }
+    }
+
+    /**
+     * Sets the sprite to the north sprite
+     */
+    private void setNORTHSprite() {
+        this.setImage(northSprite);
+    }
+
+    /**
+     * Sets the sprite to the south sprite
+     */
+    private void setSOUTHSprite() {
+        this.setImage(southSprite);
+    }
+
+    /**
+     * Sets the sprite to the west sprite
+     */
+    private void setWESTSprite() {
+        this.setImage(westSprite);
+    }
+
+    /**
+     * Sets the sprite to the east sprite
+     */
+    private void setEASTSprite() {
+        this.setImage(eastSprite);
+    }
+
+    /**
+     * Sets up the sprites for the entity for each direction
+     */
+    private void setupSprites() {
+        String imagePathNorth = String.format("%s%s_%s.png", imagePath, attackType, "NORTH");
+        String imagePathSouth = String.format("%s%s_%s.png", imagePath, attackType, "SOUTH");
+        String imagePathWest = String.format("%s%s_%s.png", imagePath, attackType, "WEST");
+        String imagePathEast = String.format("%s%s_%s.png", imagePath, attackType, "EAST");
+        northSprite = new Image(imagePathNorth);
+        southSprite = new Image(imagePathSouth);
+        westSprite = new Image(imagePathWest);
+        eastSprite = new Image(imagePathEast);
+    }
+
+    /**
+     * Returns the attackType of the AttackView
+     * @return attackType
+     */
+    public String getAttackType() {
+        return attackType;
+    }
+}
