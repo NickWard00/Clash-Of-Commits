5850003ae16998513cd765517271373d63a25201
James Qu <jq46@duke.edu>
2022-11-21 03:31:43 -0500

    Starting to handle collisions in controller - pair programming (James Qu, Mayari Merchant)

--- src/main/java/ooga/view/BlockView.java
+++ src/main/java/ooga/view/BlockView.java
@@ -4,13 +4,18 @@ import javafx.scene.control.ScrollPane;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
+import ooga.model.obstacle.Obstacle;
 
 public class BlockView{
     private ImageView imageView;
     private String imagePath;
+    private double xPos;
+    private double yPos;
 
     public BlockView(int x, int y, int blockSize, String imagePath, GridPane root){
         this.imagePath = imagePath;
+        this.xPos = x;
+        this.yPos = y;
         imageView = new ImageView(new Image(imagePath));
         imageView.setFitWidth(blockSize);
         imageView.setFitHeight(blockSize);
@@ -23,5 +28,12 @@ public class BlockView{
 
     public ImageView getImageView() {
         return this.imageView;
+    }
+    public double getXPosition() {
+        return this.xPos;
+    }
+
+    public double getYPosition() {
+        return this.yPos;
     }
 }
