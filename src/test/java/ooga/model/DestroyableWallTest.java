efca98396f8d8e115cf15f102b63bf27cff0f36e
James Qu <jq46@duke.edu>
2022-11-21 23:04:43 -0500

    Added some obstacle collision tests

--- src/test/java/ooga/model/DestroyableWallTest.java
+++ src/test/java/ooga/model/DestroyableWallTest.java
@@ -4,7 +4,11 @@ import static ooga.model.obstacle.DestroyableWall.DEFAULT_HP;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 
+import java.util.HashMap;
+import java.util.Map;
+import ooga.model.hero.MainHero;
 import ooga.model.obstacle.DestroyableWall;
+import ooga.model.state.MovementState;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -13,6 +17,8 @@ class DestroyableWallTest {
   private static final double TEST_DEFAULT_Y_POSITION = 1.0;
   private DestroyableWall defaultWall;
   private DestroyableWall wall;
+  private Entity entity;
+  private Map<String, String> attributes;
   private int testHP;
 
   @BeforeEach
@@ -32,10 +38,21 @@ class DestroyableWallTest {
     assertEquals(testHP, wall.determineHP());
   }
 
-  //TODO: Implement test here once figure out what to do with the block method
   @Test
   void testBlock() {
-
+    attributes = new HashMap<>();
+    attributes.put("XPosition", "1.0");
+    attributes.put("YPosition", "1.0");
+    attributes.put("Type", "Hero");
+    attributes.put("Speed", "100");
+    attributes.put("Size", "30");
+    attributes.put("HP", "100");
+    attributes.put("Attack", "LongRange");
+    attributes.put("Direction", "SOUTH");
+    attributes.put("Movement", "MOVING");
+    entity = new MainHero(attributes);
+    defaultWall.block(entity);
+    assertEquals(MovementState.STATIONARY.getMovement(), entity.getStateStrings().get(1));
   }
 
   @Test
--- src/test/java/ooga/model/ImmovableWallTest.java
+++ src/test/java/ooga/model/ImmovableWallTest.java
@@ -2,13 +2,19 @@ package ooga.model;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.HashMap;
+import java.util.Map;
+import ooga.model.hero.MainHero;
 import ooga.model.obstacle.ImmovableWall;
+import ooga.model.state.MovementState;
 import org.junit.jupiter.api.Test;
 
 class ImmovableWallTest {
   private static final double TEST_X_POSITION = 1.0;
   private static final double TEST_Y_POSITION = 1.0;
   private ImmovableWall wall;
+  private Map<String, String> attributes;
+  private Entity entity;
 
 
   @Test
@@ -21,6 +27,20 @@ class ImmovableWallTest {
   //TODO: Implement test here once figure out what to do with the block method
   @Test
   void testWallBlock() {
+    wall = new ImmovableWall(TEST_X_POSITION, TEST_Y_POSITION);
+    attributes = new HashMap<>();
+    attributes.put("XPosition", "1.0");
+    attributes.put("YPosition", "1.0");
+    attributes.put("Type", "Hero");
+    attributes.put("Speed", "100");
+    attributes.put("Size", "30");
+    attributes.put("HP", "100");
+    attributes.put("Attack", "LongRange");
+    attributes.put("Direction", "SOUTH");
+    attributes.put("Movement", "MOVING");
+    entity = new MainHero(attributes);
+    wall.block(entity);
+    assertEquals(MovementState.STATIONARY.getMovement(), entity.getStateStrings().get(1));
 
   }
 }
