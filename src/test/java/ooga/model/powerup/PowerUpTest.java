1cf50b67db1307a87ad7150ba30e35d52c51fc98
Nick Ward <nw121@duke.edu>
2022-12-12 16:10:29 -0500

    new save file parser test

--- src/test/java/ooga/model/powerup/PowerUpTest.java
+++ src/test/java/ooga/model/powerup/PowerUpTest.java
@@ -4,7 +4,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import ooga.controller.EntityParser;
 import ooga.model.hero.MainHero;
-import ooga.model.obstacle.powerup.PowerUp;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -16,12 +15,12 @@ class PowerUpTest {
   private MainHero hero;
 
   private static final int TEST_HP = 1;
-  private static final double TEST_DEFAULT_X_POSITION = 1.0;
-  private static final double TEST_DEFAULT_Y_POSITION = 1.0;
+  private static final int TEST_DEFAULT_X_POSITION = 1;
+  private static final int TEST_DEFAULT_Y_POSITION = 1;
 
   @BeforeEach
   void setUp() {
-    powerUp = new PowerUp(TEST_DEFAULT_X_POSITION, TEST_DEFAULT_Y_POSITION);
+    powerUp = new HealPowerUp("test1", TEST_DEFAULT_X_POSITION, TEST_DEFAULT_Y_POSITION);
     heroParser = new EntityParser("TestHero", new String[]{"MainHero", "5", "5"});
     hero = new MainHero(heroParser.getAttributeMap());
   }
@@ -29,8 +28,8 @@ class PowerUpTest {
   @Test
   void testUpdateHP() {
     int hpBefore = hero.getHp();
-    powerUp.upgradeHP(hero, TEST_HP);
-    assertEquals(hpBefore + TEST_HP, hero.getHp());
+    powerUp.activatePowerUp(hero);
+    assertEquals(hpBefore + 1, hero.getHp());
   }
 
 }
